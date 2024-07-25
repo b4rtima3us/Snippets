77a4822241aa62dbb1f75ce3672faa1d227555be
G. Volk-Karachevskiy <bartima3us@yandex.ru>
2024-07-25 20:47:17 +0300

    add public field

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Textarea, TextInput
+from django.forms import ModelForm, Textarea, TextInput, CheckboxInput
 from MainApp.models import Snippet
 
 
@@ -6,12 +6,13 @@ class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
-        fields = ['name', 'lang', 'code']
-        labels = {'name': '', 'lang': '', 'code': ''}
+        fields = ['name', 'lang', 'public', 'code']
+        labels = {'name': '', 'lang': '', 'public': '', 'code': ''}
         widgets = {
             'name': TextInput(attrs={
                 "class": "form-control", 'style': 'max-width: 350px', 'placeholder': 'Название сниппета'
             }),
+            'public': CheckboxInput(attrs={'id': 'public'}),
             'code': Textarea(attrs={
                 "class": "form-control", 'style': 'max-width: 350px', 'placeholder': 'Код сниппета'
             })
