58d867f29535d09a97bd46feff3c02fe5f6c307e
G. Volk-Karachevskiy <bartima3us@yandex.ru>
2024-07-25 12:53:53 +0300

    add forms

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect, HttpResponse
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -10,8 +11,22 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    # создаем пустую форму при запросе гет
+    if request.method == 'GET':
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form
+        }
+        return render(request, 'pages/add_snippet.html', context)
+
+    # получаем данные из формы и создаем новый сниппет в бд
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippet_list")  # GET snippets/list
+        return render(request, 'pages/add_snippet.html', {'form': form})
 
 
 def snippets_page(request):
@@ -26,4 +41,3 @@ def render_snippet(request, item_id):
         return render(request, 'pages/snippet_page.html', {'item': item})
     except ObjectDoesNotExist:
         return HttpResponse("Snippet not found")
-
